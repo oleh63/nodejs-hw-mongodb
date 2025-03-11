35cf4ed88172ed8daf768cb188b38d5a74744246
Oleh Skrypets <olehskrupets@gmail.com>
2025-03-11 18:03:25 +0200

    add fix error

--- src/server.js
+++ src/server.js
@@ -54,6 +54,11 @@ export const setupServer = () => {
     res.status(404).json({ message: 'Not found' });
   });
 
+  app.use((err, req, res, next) => {
+    console.error('Server Error:', err);
+    res.status(500).json({ message: 'Internal Server Error' });
+  });
+
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
