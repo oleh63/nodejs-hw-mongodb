cc54def2a7d83beb82d8f14720bc98cebe548773
Oleh Skrypets <olehskrupets@gmail.com>
2025-04-13 10:39:10 +0300

    add all steps

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
+import * as fs from 'node:fs';
 import path from 'node:path';
 
 import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
 import cookieParser from 'cookie-parser';
+import swaggerUIExpress from 'swagger-ui-express';
 
 import router from './routes/index.js';
 import { getEnvVar } from './utils/getEnvVar.js';
@@ -12,8 +14,18 @@ import { errorHandler } from './middlewares/errorHandler.js';
 
 const PORT = Number(getEnvVar('PORT', '3000'));
 
+const swaggerDocument = JSON.parse(
+  fs.readFileSync(path.resolve('docs', 'swagger.json'), 'utf-8'),
+);
+
 export const setupServer = () => {
   const app = express();
+
+  app.use(
+    '/api-docs',
+    swaggerUIExpress.serve,
+    swaggerUIExpress.setup(swaggerDocument),
+  );
 
   app.use('/uploads', express.static(path.resolve('src', 'uploads')));
 
